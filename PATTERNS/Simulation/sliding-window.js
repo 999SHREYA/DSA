b5c92a23c4555e2c8203f3fd6fa3d4f12bd107b8
SHREYA <mishrashreya900@gmail.com>
2026-08-14 20:39:14 +0530

    solution2 - sliding-window

--- PATTERNS/Simulation/sliding-window.js
+++ PATTERNS/Simulation/sliding-window.js
@@ -38,3 +38,42 @@ function maxSubarrayLength(nums, k) {
 console.log(
     maxSubarrayLength([1, 2, 3, 1, 2, 3, 1, 2], 2)
 ); // 6
+
+
+
+// solution 2
+
+function maximumLengthSubstring(s) {
+    let left = 0;
+    let maxLength = 0;
+
+    const freq = new Map();
+
+    for (let right = 0; right < s.length; right++) {
+        const ch = s[right];
+
+        freq.set(ch, (freq.get(ch) || 0) + 1);
+
+        while (freq.get(ch) > 2) {
+            const leftChar = s[left];
+
+            freq.set(
+                leftChar,
+                freq.get(leftChar) - 1
+            );
+
+            left++;
+        }
+
+        maxLength = Math.max(
+            maxLength,
+            right - left + 1
+        );
+    }
+
+    return maxLength;
+}
+
+// Test
+console.log(maximumLengthSubstring("bcbbbcba")); // 4
+console.log(maximumLengthSubstring("aaaa"));     // 2
